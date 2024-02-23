1fa5e282bb239c5aae818155feb42b5065ffbf85
HorsepowerJoe <ljw103302@naver.com>
2024-02-23 15:28:50 +0900

    4.2.137

--- src/main/java/sia/tacocloud/tacos/controller/HomeController.java
+++ src/main/java/sia/tacocloud/tacos/controller/HomeController.java
@@ -2,9 +2,6 @@ package sia.tacocloud.tacos.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
--- src/main/java/sia/tacocloud/tacos/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/tacos/security/SecurityConfig.java
@@ -6,13 +6,17 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.support.BaseLdapPathContextSource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.ldap.userdetails.DefaultLdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.LdapAuthoritiesPopulator;
+import org.springframework.security.ldap.userdetails.PersonContextMapper;
 import org.springframework.security.web.SecurityFilterChain;
 
 import lombok.RequiredArgsConstructor;
@@ -45,12 +49,40 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // @Bean
+    // public JdbcUserDetailsManager jdbcUserDetailsService() {
+    // PasswordEncoder passwordEncoder = passwordEncoder();
+    // UserDetails user =
+    // User.withUsername("user1").password("password1").roles("USER").passwordEncoder(passwordEncoder::encode).build();
+    // JdbcUserDetailsManager jdbcUserDetailsManager = new
+    // JdbcUserDetailsManager(dataSource);
+    // jdbcUserDetailsManager.createUser(user);
+    // return jdbcUserDetailsManager;
+    // }
     @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsService() {
-        PasswordEncoder passwordEncoder = passwordEncoder();
-        UserDetails user = User.withUsername("user1").password("password1").roles("USER").passwordEncoder(passwordEncoder::encode).build();
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        jdbcUserDetailsManager.createUser(user);
-        return jdbcUserDetailsManager;
+    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
+        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean = EmbeddedLdapServerContextSourceFactoryBean
+                .fromEmbeddedLdapServer();
+        contextSourceFactoryBean.setPort(8389);
+        contextSourceFactoryBean.setRoot("dc=tacocloud,dc=com");
+        return contextSourceFactoryBean;
+    }
+
+    @Bean
+    LdapAuthoritiesPopulator authorities(BaseLdapPathContextSource contextSource) {
+        String groupSearchBase = "ou=groups";
+        DefaultLdapAuthoritiesPopulator authorities = new DefaultLdapAuthoritiesPopulator(contextSource,
+                groupSearchBase);
+        authorities.setGroupSearchFilter("(member={0})");
+        return authorities;
+    }
+
+    @Bean
+    AuthenticationManager ldapAuthenticationManager(
+            BaseLdapPathContextSource contextSource) {
+        LdapBindAuthenticationManagerFactory factory = new LdapBindAuthenticationManagerFactory(contextSource);
+        factory.setUserSearchBase("ou=people");
+        factory.setUserSearchFilter("(uid={0})");
+        return factory.createAuthenticationManager();
     }
 }
